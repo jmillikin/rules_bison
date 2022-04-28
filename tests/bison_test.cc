f90879b16229a615f0225f03739007c3c8d882f6
John Millikin <john@john-millikin.com>
2022-04-28 16:31:34 +0900

    Use environment variables to locate test data.
    GitOrigin-RevId: c0097bdb03f38b94efdc9854a5b3e360529c2a5c

--- tests/bison_test.cc
+++ tests/bison_test.cc
@@ -32,26 +32,51 @@ class RulesBison : public ::testing::Test {
         return buf.str();
     }
 
+    string TestsDir() {
+        const char *test_binary_ptr = getenv("TEST_BINARY");
+        const char *test_workspace_ptr = getenv("TEST_WORKSPACE");
+
+        if (test_binary_ptr == nullptr) {
+            EXPECT_NE(test_binary_ptr, nullptr);
+            return "";
+        }
+        if (test_workspace_ptr == nullptr) {
+            EXPECT_NE(test_workspace_ptr, nullptr);
+            return "";
+        }
+
+        string test_binary(test_binary_ptr);
+        string test_workspace(test_workspace_ptr);
+
+        size_t slash = test_binary.find_last_of('/');
+        if (slash == string::npos) {
+            EXPECT_NE(slash, string::npos);
+            return "";
+        }
+
+        return test_workspace + "/" + test_binary.substr(0, slash);
+    }
+
     std::unique_ptr<Runfiles> runfiles_;
 };
 
 TEST_F(RulesBison, GenruleTest) {
-    const auto parser_src = ReadFile("rules_bison/tests/genrule_output.c");
+    const auto parser_src = ReadFile(TestsDir() + "/genrule_output.c");
     ASSERT_THAT(parser_src, HasSubstr("Bison implementation for Yacc-like parsers in C"));
 }
 
 TEST_F(RulesBison, CompiledParserC) {
-    const auto hello_c_bin = ReadFile("rules_bison/tests/hello_c_bin" EXE);
+    const auto hello_c_bin = ReadFile(TestsDir() + "/hello_c_bin" EXE);
     ASSERT_TRUE(hello_c_bin.size() > 0);
 }
 
 TEST_F(RulesBison, CompiledParserCxx) {
-    const auto hello_cc_bin = ReadFile("rules_bison/tests/hello_cc_bin" EXE);
+    const auto hello_cc_bin = ReadFile(TestsDir() + "/hello_cc_bin" EXE);
     ASSERT_TRUE(hello_cc_bin.size() > 0);
 }
 
 TEST_F(RulesBison, CompiledParserJava) {
-    const auto hello_java_bin = ReadFile("rules_bison/tests/HelloJavaMain.jar");
+    const auto hello_java_bin = ReadFile(TestsDir() + "/HelloJavaMain.jar");
     ASSERT_TRUE(hello_java_bin.size() > 0);
 
 }
